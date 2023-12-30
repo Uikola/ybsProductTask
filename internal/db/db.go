c4ba8bc1ae15fc4ee95f429ba49aa4fc7b70f14d
Yura <ugulaev806@yandex.ru>
2023-12-30 10:59:06 +0400

    add InitDB function

---
+++ internal/db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"database/sql"
+	"github.com/Uikola/ybsProductTask/internal/config"
+	sl "github.com/Uikola/ybsProductTask/internal/lib/logger"
+	"log/slog"
+)
+
+func InitDB(cfg *config.Config, log *slog.Logger) *sql.DB {
+	db, err := sql.Open(cfg.DriverName, cfg.ConnString)
+	if err != nil {
+		log.Info("failed to connect to the database", sl.Err(err))
+		return nil
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Info("failed to ping the database", sl.Err(err))
+		return nil
+	}
+	return db
+}
