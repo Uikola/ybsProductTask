45c61a61c54b03f6ed73abff944d8667ca310763
Yura <ugulaev806@yandex.ru>
2023-12-30 10:57:35 +0400

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,24 +1,34 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"time"
+)
 
 type Config struct {
-	Port       string `mapstructure:"PORT"`
-	ConnString string `mapstructure:"CONN_STRING"`
-	DriverName string `mapstructure:"DRIVER_NAME"`
+	Port        string        `mapstructure:"PORT"`
+	ConnString  string        `mapstructure:"CONN_STRING"`
+	DriverName  string        `mapstructure:"DRIVER_NAME"`
+	Env         string        `mapstructure:"ENV"`
+	Timeout     time.Duration `mapstructure:"TIMEOUT"`
+	IdleTimeout time.Duration `mapstructure:"IDLE_TIMEOUT"`
 }
 
-func LoadConfig() (config Config, err error) {
+func MustConfig() *Config {
+	var config Config
 	viper.AddConfigPath("./internal/config/envs")
 	viper.SetConfigName("dev")
 	viper.SetConfigType("env")
 	viper.AutomaticEnv()
 
-	err = viper.ReadInConfig()
+	err := viper.ReadInConfig()
 	if err != nil {
-		return
+		panic("failed to read config: " + err.Error())
 	}
 
 	err = viper.Unmarshal(&config)
-	return
+	if err != nil {
+		panic(err.Error())
+	}
+	return &config
 }
