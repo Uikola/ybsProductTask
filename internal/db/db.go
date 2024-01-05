a9cd706655dc19d86dca99042c5857227858c31c
uikola <ugulaev806@yandex.com>
2024-01-05 16:21:14 +0400

    remake handler.go and change logger in the db.go

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,20 +3,19 @@ package db
 import (
 	"database/sql"
 	"github.com/Uikola/ybsProductTask/internal/config"
-	sl "github.com/Uikola/ybsProductTask/internal/src/logger"
-	"log/slog"
+	"github.com/rs/zerolog"
 )
 
-func InitDB(cfg *config.Config, log *slog.Logger) *sql.DB {
+func InitDB(cfg *config.Config, log zerolog.Logger) *sql.DB {
 	db, err := sql.Open(cfg.DriverName, cfg.ConnString)
 	if err != nil {
-		log.Info("failed to connect to the database", sl.Err(err))
+		log.Info().Err(err).Msg("failed to connect to the database")
 		return nil
 	}
 
 	err = db.Ping()
 	if err != nil {
-		log.Info("failed to ping the database", sl.Err(err))
+		log.Info().Err(err).Msg("failed to ping the database")
 		return nil
 	}
 	return db
--- internal/server/handler.go
+++ internal/server/handler.go
@@ -2,31 +2,42 @@ package server
 
 import (
 	"database/sql"
+
 	"github.com/Uikola/ybsProductTask/internal/db/repository/postgres"
 	"github.com/Uikola/ybsProductTask/internal/server/courier"
 	"github.com/Uikola/ybsProductTask/internal/server/order"
 	"github.com/Uikola/ybsProductTask/internal/usecase/courier_usecase"
 	"github.com/Uikola/ybsProductTask/internal/usecase/order_usecase"
 	"github.com/go-chi/chi/v5"
-	"log/slog"
+	"github.com/rs/zerolog"
 )
 
-func Router(db *sql.DB, router chi.Router, log *slog.Logger) {
+type Handler struct {
+	Courier courier.Handler
+	Order   order.Handler
+}
+
+func New(courierHandler courier.Handler, orderHandler order.Handler) *Handler {
+	return &Handler{Courier: courierHandler, Order: orderHandler}
+}
+
+func Router(db *sql.DB, router chi.Router, log zerolog.Logger) {
 	courierRepository := postgres.NewCourierRepository(db)
 	orderRepository := postgres.NewOrderRepository(db)
 	courierUseCase := courier_usecase.New(courierRepository, orderRepository)
 	orderUseCase := order_usecase.New(orderRepository)
+	handler := New(courier.New(courierUseCase, log), order.New(orderUseCase, log))
 
 	router.Route("/couriers", func(r chi.Router) {
-		r.Post("/", courier.LoadCouriers(courierUseCase, log))
-		r.Get("/{courier_id}", courier.GetCourier(courierUseCase, log))
-		r.Get("/", courier.GetCouriers(courierUseCase, log))
-		r.Get("/meta-info/{courier_id}", courier.GetMetaInfo(courierUseCase, log))
+		r.Post("/", handler.Courier.LoadCouriers)
+		r.Get("/{courier_id}", handler.Courier.GetCourier)
+		r.Get("/", handler.Courier.GetCouriers)
+		r.Get("/meta-info/{courier_id}", handler.Courier.GetMetaInfo)
 	})
 	router.Route("/orders", func(r chi.Router) {
-		r.Post("/", order.LoadOrders(orderUseCase, log))
-		r.Get("/{order_id}", order.GetOrder(orderUseCase, log))
-		r.Get("/", order.GetOrders(orderUseCase, log))
-		r.Put("/complete", order.CompleteOrder(orderUseCase, log))
+		r.Post("/", handler.Order.LoadOrders)
+		r.Get("/{order_id}", handler.Order.GetOrder)
+		r.Get("/", handler.Order.GetOrders)
+		r.Put("/complete", handler.Order.CompleteOrder)
 	})
 }
